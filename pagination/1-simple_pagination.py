19002fc5d61ab249dc9a0f7f849d21b54aead2bf
ABDULLAHALSALEM <Abdullah.m122144@gmail.com>
2026-03-07 17:28:53 +0300

    add task 1

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -4,24 +4,27 @@ Simple pagination module
 """
 
 import csv
-import importlib
 from typing import List
 
-# Import from module with name starting with digit
-helper_module = importlib.import_module('0-simple_helper_function')
-index_range = helper_module.index_range
+
+def index_range(page: int, page_size: int) -> tuple:
+    """Return start and end indexes for pagination."""
+    start = (page - 1) * page_size
+    end = page * page_size
+    return (start, end)
 
 
 class Server:
-    """Server class to paginate a database of baby names"""
+    """Server class to paginate a database of popular baby names."""
 
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Initialize the server."""
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Load and cache dataset"""
+        """Cached dataset."""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -30,14 +33,10 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Return a page of the dataset"""
-
+        """Return the appropriate page of the dataset."""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
         start, end = index_range(page, page_size)
-
-        data = self.dataset()
-
-        return data[start:end]
-    
+        dataset = self.dataset()
+        return dataset[start:end]
