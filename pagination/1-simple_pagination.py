3ff9db9df1e52a6bb3d14c9274d7aece0a1a5b43
ABDULLAHALSALEM <Abdullah.m122144@gmail.com>
2026-03-07 17:48:20 +0300

    fiex

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """
-Return the dataset loaded from the CSV file and cache it.
+Simple pagination module
 """
-
 
 import csv
 from typing import List
 
+
 def index_range(page: int, page_size: int) -> tuple:
-    """Return start and end index for a pagination page."""
+    """Return a tuple containing the start and end indexes for pagination."""
     start = (page - 1) * page_size
     end = page * page_size
     return (start, end)
@@ -24,7 +24,7 @@ class Server:
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Return cached dataset."""
+        """Return the dataset loaded from the CSV file and cache it."""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -33,13 +33,15 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Return the requested page from the dataset."""
+        """Return the appropriate page of the dataset."""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
         start, end = index_range(page, page_size)
         data = self.dataset()
+
         if start >= len(data):
             return []
+
         return data[start:end]
     
